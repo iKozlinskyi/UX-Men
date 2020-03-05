12bc64796217499a859de22f8244180e1d76b237
Ihor Kozlinskyi <Ihor_Kozlinskyi@epam.com>
2020-03-05 14:50:22 +0200

    Configure server for deploy

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const port = process.env.PORT || 5050;
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen('5050', () => console.log('app is listening on 5050'));
+app.listen(port, () => console.log('app is listening on 5050'));
