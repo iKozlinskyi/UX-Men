fb2d947f772c33eeb2aaf7f143134feeacaad705
Ihor Kozlinskyi <ikozlinskyi@gmail.com>
2020-03-10 14:41:27 +0200

    Fix regex in file scan

--- utils/generateHtmlPlugins.js
+++ utils/generateHtmlPlugins.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 function generateHtmlPlugins(templateDir, shouldMinify) {
     const templateFiles = fileTraversal(templateDir).filter(file => file.endsWith('.html') || file.endsWith('.ejs'));
     return templateFiles.map(item => {
-        const parts = item.split(/[\.\\]/);
+        const parts = item.split(/[\/\.\\]/);
         const extension = parts[parts.length - 1];
         const fileName = parts[parts.length - 2];
         const chunkName = parts[parts.length - 3];
