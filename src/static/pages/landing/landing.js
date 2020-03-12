1911165b5d57b1512b6b95abb0b93ba47bddc74e
Ihor Kozlinskyi <ikozlinskyi@gmail.com>
2020-03-12 18:51:57 +0200

    comment out code

--- src/static/pages/landing/landing.js
+++ src/static/pages/landing/landing.js
@@ -30,88 +30,88 @@ import {getPollResults} from "../../../../utils/getPollResults";
 import {sendNewPollResults} from "../../../../utils/sendNewPollResults";
 /*Timer*/
 
-export let timer = setInterval(function() {
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  let countDown = new Date("June 01, 2020 00:00:00").getTime();
-
-  let now = new Date().getTime();
-  let distance = countDown - now;
-
-  document.getElementById("days").innerText = Math.floor(distance / day);
-  document.getElementById("hours").innerText = Math.floor(
-    (distance % day) / hour
-  );
-  document.getElementById("minutes").innerText = Math.floor(
-    (distance % hour) / minute
-  );
-  document.getElementById("seconds").innerText = Math.floor(
-    (distance % minute) / second
-  );
-
-  if (distance < 0) {
-    clearInterval(timer);
-    const deadlineWrapper = document.getElementsByClassName("deadline")[0];
-    document.body.removeChild(deadlineWrapper);
-  }
-}, 1000);
+// export let timer = setInterval(function() {
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
+//
+//   let countDown = new Date("June 01, 2020 00:00:00").getTime();
+//
+//   let now = new Date().getTime();
+//   let distance = countDown - now;
+//
+//   document.getElementById("days").innerText = Math.floor(distance / day);
+//   document.getElementById("hours").innerText = Math.floor(
+//     (distance % day) / hour
+//   );
+//   document.getElementById("minutes").innerText = Math.floor(
+//     (distance % hour) / minute
+//   );
+//   document.getElementById("seconds").innerText = Math.floor(
+//     (distance % minute) / second
+//   );
+//
+//   if (distance < 0) {
+//     clearInterval(timer);
+//     const deadlineWrapper = document.getElementsByClassName("deadline")[0];
+//     document.body.removeChild(deadlineWrapper);
+//   }
+// }, 1000);
 
 /*Print text*/
 
-let messageCount = 0;
-
-const deadlineTitleMessage = "Will they make it before the deadline?";
-const deadlineTitle = document.getElementsByClassName("deadline__title")[0];
-let timerDeadlineTitle;
-
-export function printText(message, messageWrapper, timer) {
-  messageWrapper.innerHTML = message.substring(0, messageCount);
-  if (messageCount === message.length) {
-    clearInterval(timer);
-  } else {
-    messageCount++;
-  }
-}
-
-window.addEventListener("scroll", function() {
-  let messageTop = deadlineTitle.getBoundingClientRect().top;
-
-  if (timerDeadlineTitle) {
-    return;
-  }
-
-  if (messageTop <= 250) {
-    timerDeadlineTitle = setInterval(
-      () => printText(deadlineTitleMessage, deadlineTitle, timerDeadlineTitle),
-      90
-    );
-  }
-});
-
-/* Go to About page */
-const teamPageButton = document.querySelector(".team__button--clicked");
-
-teamPageButton.addEventListener("click", function() {
-  window.location.pathname = "/about";
-});
-
-
-window.onload = function() {
-  const canvasDiv = document.getElementById("particle_canvas");
-
-  const options = {
-    particleColor: "#6cd1eb",
-    background: "",
-    interactive: true,
-    speed: "medium",
-    density: "high",
-  };
-
-  new ParticleNetwork(canvasDiv, options);
-};
+// let messageCount = 0;
+//
+// const deadlineTitleMessage = "Will they make it before the deadline?";
+// const deadlineTitle = document.getElementsByClassName("deadline__title")[0];
+// let timerDeadlineTitle;
+//
+// export function printText(message, messageWrapper, timer) {
+//   messageWrapper.innerHTML = message.substring(0, messageCount);
+//   if (messageCount === message.length) {
+//     clearInterval(timer);
+//   } else {
+//     messageCount++;
+//   }
+// }
+//
+// window.addEventListener("scroll", function() {
+//   let messageTop = deadlineTitle.getBoundingClientRect().top;
+//
+//   if (timerDeadlineTitle) {
+//     return;
+//   }
+//
+//   if (messageTop <= 250) {
+//     timerDeadlineTitle = setInterval(
+//       () => printText(deadlineTitleMessage, deadlineTitle, timerDeadlineTitle),
+//       90
+//     );
+//   }
+// });
+//
+// /* Go to About page */
+// const teamPageButton = document.querySelector(".team__button--clicked");
+//
+// teamPageButton.addEventListener("click", function() {
+//   window.location.pathname = "/about";
+// });
+//
+//
+// window.onload = function() {
+//   const canvasDiv = document.getElementById("particle_canvas");
+//
+//   const options = {
+//     particleColor: "#6cd1eb",
+//     background: "",
+//     interactive: true,
+//     speed: "medium",
+//     density: "high",
+//   };
+//
+//   new ParticleNetwork(canvasDiv, options);
+// };
 
 // /*Background-gradient animation*/
 const backgroundRoot =  document.querySelector('.pick-side__background-root');
