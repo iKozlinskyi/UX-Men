f937faf02cb60992e1f7b3d66ace927b3798b940
iKozlinskyi <ikozlinskyi@gmail.com>
2020-03-13 11:49:20 +0200

    fix white stripe in dark mode

--- src/static/pages/serverman/serverman.js
+++ src/static/pages/serverman/serverman.js
@@ -1,7 +1,7 @@
 import {toggleTheme} from "../../shared/utils/toggleTheme";
 
 const servermanDarkThemedClasses = ['superhero', 'superhero__blockquote', 'superhero__story',
-  'profile', 'profile__entry', 'connections', 'theme-toggle__icon'];
+  'profile', 'profile__entry', 'connections', 'theme-toggle__icon', 'introduction'];
 
 const themeToggle = document.querySelector('.theme-toggle__button');
 
