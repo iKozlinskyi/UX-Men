af90b58d47836c46863da7a209de12c83b30f635
iKozlinskyi <ikozlinskyi@gmail.com>
2020-03-13 11:27:34 +0200

    improve theme toggle button look

--- src/static/pages/serverman/serverman.js
+++ src/static/pages/serverman/serverman.js
@@ -1,11 +1,10 @@
 import {toggleTheme} from "../../shared/utils/toggleTheme";
 
 const servermanDarkThemedClasses = ['superhero', 'superhero__blockquote', 'superhero__story',
-  'profile', 'profile__entry', 'connections'];
+  'profile', 'profile__entry', 'connections', 'theme-toggle__icon'];
 
 const themeToggle = document.querySelector('.theme-toggle__button');
 
 themeToggle.addEventListener('click', () => {
     toggleTheme('dark', servermanDarkThemedClasses);
 });
-
--- src/static/shared/utils/toggleTheme.js
+++ src/static/shared/utils/toggleTheme.js
@@ -4,7 +4,7 @@ export const toggleTheme = (themeName, classNames) => {
     classNames.forEach(className => {
         let elementsWithClassName = [...document.getElementsByClassName(className)];
         elementsWithClassName.forEach((element) => {
-            element.style.transition = `all ${transitionTime / 1000}s ease-out`;
+            element.style.transition = `background-color ${transitionTime / 1000}s ease-out`;
             element.classList.toggle(className + '--' + themeName);
             setTimeout(() => { element.style.transition = ''; }, transitionTime)
         })
